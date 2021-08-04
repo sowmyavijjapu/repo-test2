cf87f90337e8d9aa1df54f6df8908ffc009424c2
sowmyavijjapu <87028325+sowmyavijjapu@users.noreply.github.com>
2021-08-04 08:39:48 +0530

    Github change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! second change" );
+        System.out.println( "Hello World! Github hook change" );
     }
 }
